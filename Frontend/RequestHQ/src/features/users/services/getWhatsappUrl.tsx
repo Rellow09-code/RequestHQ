0fa28b245cc39d7e9d11e720268cd8afc8db4bb6
Rellow09-code <2769364@students.wits.ac.za>
2026-07-04 12:13:10 +0200

    Deployment: Successful

--- Frontend/RequestHQ/src/features/users/services/getWhatsappUrl.tsx
+++ Frontend/RequestHQ/src/features/users/services/getWhatsappUrl.tsx
@@ -1,5 +1,6 @@
 export default function getWhatsappUrl(target_phone:string, message:string){
     const encoded_message = encodeURIComponent(message);
     const url = `https://wa.me/${target_phone}?text=${encoded_message}`;
+    console.log(url)
     return `Successfully set to whatsapp!`
 }
